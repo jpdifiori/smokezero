af1718422051ac29fde169054acc8dca68488e62
Juan Pablo Di Fiori <juanpablodifiori@Juans-MacBook-Pro.local>
2026-01-17 07:41:44 -0300

    fix: hide bottom nav and restore fab position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
           {user && (
             <>
               <GuardianFAB />
-              <BottomNav />
+              {/* <BottomNav /> */}
             </>
           )}
         </StatsProvider>
